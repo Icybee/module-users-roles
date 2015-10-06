764a8ea672055f025429c906e7185cb98031ffb9
Olivier Laviale <olivier.laviale@gmail.com>
2015-10-06 23:42:26 +0200

    icanboogie/routing update

--- config/routes.php
+++ config/routes.php
@@ -8,6 +8,6 @@ use Icybee\Routing\RouteMaker as Make;
 return Make::admin('users.roles', Routing\RolesAdminController::class, [
 
 	'id_name' => 'rid',
-	'only' => [ 'index', 'create', 'edit', 'confirm-delete' ]
+	'only' => [ Make::ACTION_INDEX, Make::ACTION_NEW, Make::ACTION_EDIT, Make::ACTION_CONFIRM_DELETE ]
 
 ]);
